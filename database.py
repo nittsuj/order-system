1dc5a7cced8d5efde1bc640efd039f0fdad1724e
justinvalentino <justinvalentino88@gmail.com>
2026-01-16 09:53:07 +0700

    fix: typo

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URl_DATABASE= ''
+URL_DATABASE= ''
 
 engine = create_engine(URL_DATABASE)
 
