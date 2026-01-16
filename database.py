4a7c78b0593697b6b569cb099ba628fdc0f86871
nittsuj <justinvalentino88@gmail.com>
2026-01-16 12:23:51 +0700

    add: database's url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE= ''
+URL_DATABASE= "mysql+pymysql://root:digital@db:3306/ecommerce_db"
 
 engine = create_engine(URL_DATABASE)
 
