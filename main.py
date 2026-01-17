71aaa8ec3fdd193a1ec9ec94cbee659ffc72f677
nittsuj <justinvalentino88@gmail.com>
2026-01-17 23:07:21 +0700

    Remove get_db function and Import get_db from database

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from sqlalchemy.exc import OperationalError
 
 import models
 import schemas
-from database import engine, SessionLocal
+from database import engine, SessionLocal, get_db
 
 from typing import List
 import time
+
+from worker import processing_email
 
 # Issue: Docker tries to start web and db simultaneously,
 # but db takes 5-10s
@@ -20,13 +22,6 @@ while True:
     except OperationalError as e:
         print("TRYING")
         time.sleep(2)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 app = FastAPI()
 
