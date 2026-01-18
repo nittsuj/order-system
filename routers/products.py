272ea92194fe5d3de1b3e4da601c5f784d74cf5f
nittsuj <justinvalentino88@gmail.com>
2026-01-18 10:30:03 +0700

    Moved POST & GET products from main

--- routers/products.py
+++ routers/products.py
@@ -3,11 +3,32 @@ from sqlalchemy.orm import Session
 from database import get_db
 import models
 from worker import processing_order
+import schemas
 
 router = APIRouter(
     prefix="/products",
     tags=["products"]
 )
+
+@router.post("/", 
+          response_model=schemas.ProductResponse, 
+          status_code=status.HTTP_201_CREATED)
+def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
+    new_product = models.Product(
+        name=product.name,
+        price=product.price,
+        stock=product.stock
+    )
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return new_product
+
+@router.get("/", 
+         response_model=List[schemas.ProductResponse])
+def read_products(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    products = db.query(models.Product).offset(skip).limit(limit).all()
+    return products
 
 @router.post("/buy/{product_id}")
 def buy_product(product_id: int, db: Session = Depends(get_db)):
