9562568c31403640b8b02436e9564b51d651d54d
nittsuj <justinvalentino88@gmail.com>
2026-01-18 10:42:14 +0700

    Removed route for users, products, and orders and move it to routers

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ from database import engine, SessionLocal, get_db
 from typing import List
 import time
 
-from worker import processing_email
-
+from routers import products
 # Issue: Docker tries to start web and db simultaneously,
 # but db takes 5-10s
 # this prevents crashing & errors
@@ -25,66 +24,8 @@ while True:
 
 app = FastAPI()
 
-@app.post("/users/",
-          response_model=schemas.UserResponse,
-          status_code=status.HTTP_201_CREATED)
-def create_user(user: schemas.UserCreate,
-                db: Session = Depends(get_db)):
-    user_exist = db.query(models.User).filter(models.User.email == user.email).first()
-    if user_exist:
-        raise HTTPException(status_code=400, detail="Email already registered")
+app.include_router(products.router)
 
-    new_user = models.User(username=user.username, email=user.email)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-@app.get("/users/",
-         response_model=List[schemas.UserResponse])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = db.query(models.User).offset(skip).limit(limit).all()
-    return users
-
-@app.get("/users/{user_id}", 
-         response_model=schemas.UserResponse)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.id == user_id).first()
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-@app.post("/products/", 
-          response_model=schemas.ProductResponse, 
-          status_code=status.HTTP_201_CREATED)
-def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
-    new_product = models.Product(
-        name=product.name,
-        price=product.price,
-        stock=product.stock
-    )
-    db.add(new_product)
-    db.commit()
-    db.refresh(new_product)
-    return new_product
-
-@app.get("/products/", 
-         response_model=List[schemas.ProductResponse])
-def read_products(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    products = db.query(models.Product).offset(skip).limit(limit).all()
-    return products
-
-@app.post("/orders/", 
-          response_model=schemas.OrderResponse, 
-          status_code=status.HTTP_201_CREATED)
-def create_order(order: schemas.OrderCreate, db: Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.id == order.users_id).first()
-    if not db_user:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    new_order = models.Order(status=order.status, users_id=order.users_id)
-
-    db.add(new_order)
-    db.commit()
-    db.refresh(new_order)
-    return new_order
+@app.get("/")
+def root():
+    return {"message": "System Order is Running"}
