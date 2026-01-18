a859084b7d2bd5c7ab1e286c14a9af4e50873fc4
nittsuj <justinvalentino88@gmail.com>
2026-01-18 11:40:06 +0700

    Add PUT and DELETE

--- routers/products.py
+++ routers/products.py
@@ -30,6 +30,34 @@ def read_products(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
     products = db.query(models.Product).offset(skip).limit(limit).all()
     return products
 
+@router.put("/{product_id}",
+            response_model=schemas.ProductResponse)
+def update_product(product id: int, product_update: schemas.ProductCreate, db: Session=Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == product_id).first()
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    
+    product.name = product_update.name
+    product.price = product_update.price
+    product.stock = product_update.stock
+    
+    db.commit()
+    db.refresh(product)
+
+    return product
+
+@router.delete("/{product_id}",
+               status_code=status.HTTP_204_NO_CONTENT)
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.i == product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    db.delete(product)
+    db.commit()
+
+    return None
+
 @router.post("/buy/{product_id}")
 def buy_product(product_id: int, db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == product_id).with_for_update().first()
