f12c612c9ab7e16956d4de9d10ac29b9decaab0d
nittsuj <justinvalentino88@gmail.com>
2026-01-19 15:27:18 +0700

    Used environment variable instead of hardcode

--- worker.py
+++ worker.py
@@ -1,9 +1,13 @@
 from celery import Celery
 from time import sleep
+import os
+
+broker_url= os.getenv('CELERY_BROKER_URL','redis://localhost:6379/0')
+backend_url= os.getenv('CELERY_RESULT_BACKEND','redis://localhost:6379/0')
 
 app = Celery("tasks",
-             broker="redis://localhost:6379",
-             backend="redis://localhost:6379")
+             broker=broker_url,
+             backend=backend_url)
 
 @app.task
 def processing_order(order_id: int):
